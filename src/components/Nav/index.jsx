f84a2e3afad76c83f5cb9ad8dd4325c90120fbdf
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-06-04 10:23:10 -0300

    fix: scroll navbar

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -5,9 +5,9 @@ function Nav({ itens, setCategoria, categorias }) {
   return (
     <aside
       id="navbar"
-      className="fixed h-screen bg-gray-900 right-0 w-screen xl:w-1/3 md:w-1/2 translate-x-full duration-500 z-10"
+      className="fixed h-screen bg-gray-900 right-0 w-screen xl:w-1/3 md:w-1/2 translate-x-full duration-500 z-10 overflow-y-scroll"
     >
-      <div className="flex w-full justify-between items-center my-5 px-10 h-16">
+      <div className="w-full flex justify-between items-center p-10">
         <h2 className="text-gray-100 text-xl font-bold">Categorias</h2>
         <button
           className="flex justify-center items-center"
@@ -21,9 +21,9 @@ function Nav({ itens, setCategoria, categorias }) {
           <RiCloseFill className="text-gray-100 h-full" size={32} />
         </button>
       </div>
-      <ul className="w-full flex flex-col justify-center items-start gap-5 px-16">
+      <ul className="w-full flex flex-col gap-5 px-16">
         {itens.map((item) => (
-          <li className="hover:scale-105 hover:bg-gray-600 duration-500 rounded w-full flex justify-start items-center">
+          <li className="hover:scale-105 hover:bg-gray-600 duration-500 rounded w-full h-10">
             <button
               onClick={() => {
                 setCategoria([item]);
@@ -31,7 +31,7 @@ function Nav({ itens, setCategoria, categorias }) {
                   .getElementById("navbar")
                   .classList.toggle("translate-x-full");
               }}
-              className="text-gray-100 flex justify-start items-center p-2 w-full"
+              className="text-gray-100 w-full h-full flex items-center justify-start p-2"
             >
               {item[0].toUpperCase() + item.substring(1)}
             </button>
