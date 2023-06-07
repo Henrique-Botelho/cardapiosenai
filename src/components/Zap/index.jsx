6c44cad07ae859c365d50dd0801eadb70e5a848b
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-06-07 11:23:42 -0300

    Update index.jsx

--- src/components/Zap/index.jsx
+++ src/components/Zap/index.jsx
@@ -5,7 +5,7 @@ import { BsWhatsapp } from "react-icons/bs";
 function Zap() {
   return (
     <a
-      href="https://api.whatsapp.com/send?phone5511997021651&text=Quero%20reservar%20o%20meu%20pedido!"
+      href="https://api.whatsapp.com/send?phone=5511970216515&text=Quero%20reservar%20o%20meu%20pedido"
       className="fixed bottom-2 right-2 z-10 flex bg-green-500 justify-center items-center rounded-2xl p-2"
     >
       <BsWhatsapp className="text-gray-100 text-3xl" />
