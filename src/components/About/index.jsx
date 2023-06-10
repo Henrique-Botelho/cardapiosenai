610c7d8809b23fa31e3d32f41300880dac28205b
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-06-10 17:57:14 -0300

    Update index.jsx

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,7 +5,7 @@ import imagemCantina from "../../assets/fundo.png"
 function About() {
   return (
     <div className="w-full">
-      <div className="bg-gray-800 p-5 gap-2 rounded w-full">
+      <div className="bg-gray-900 p-5 gap-2 rounded w-full">
         <h2 className="text-yellow-500 tracking-wide font-cherrybomb text-3xl pb-4">Sobre</h2>
         <div className="flex flex-col sm:flex-row gap-5">
           <p className="w-full tracking-widest sm:w-1/2 text-justify text-gray-100">
