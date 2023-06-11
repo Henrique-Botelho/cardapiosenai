ddd728259c6995b93ee4eb64ec66ec2da973a471
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-06-11 10:05:10 -0300

    fix: footer

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -65,46 +65,48 @@ function Home() {
           setCategoria={setCategoria}
           itens={categorias}
         />
-        <main className="container top-20 fixed bottom-0 overflow-y-scroll flex flex-col gap-3 p-3 pb-0">
-          <Panel />
-          <About />
-          {status === true ? categoria.map((categoria, index) => {
-            return (
-              <div
-                id="conteudo"
-                className="w-full flex flex-col justify-center items-center lg:items-start"
-                key={index}
-              >
-                <h3 className="text-yellow-500 font-cherrybomb tracking-wide w-full my-3 text-3xl text-center md:text-left p-2 rounded">
-                  {categoria[0].toUpperCase() + categoria.substring(1)}
-                </h3>
-                <div className="w-full flex justify-center sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-1 flex-wrap">
-                  {produtos.map((item) => {
-                    if (item.categoria == categoria) {
-                      return (
-                        <div
-                          key={item.id}
-                          className="bg-gray-100 border w-full flex flex-col justify-center items-center rounded p-2 h-32"
-                        >
-                          <span className="w-full tracking-widest text-lg font-bold text-gray-800 opacity-80">
-                            {item.nome[0].toUpperCase() +
-                              item.nome.substring(1)}
-                          </span>
-                          <span className="text-gray-500 tracking-wide w-full h-full">
-                            {item.descricao[0].toUpperCase() +
-                              item.descricao.substring(1)}
-                          </span>
-                          <span className="w-full tracking-widest text-right whitespace-nowrap text-lg text-gray-800 font-bold opacity-80">
-                            R$ {item.preco.toFixed(2).replace(".", ",")}
-                          </span>
-                        </div>
-                      );
-                    }
-                  })}
+        <main className="container top-20 fixed bottom-0 overflow-y-scroll flex flex-col gap-3 pb-0">
+          <div className="w-full p-3">
+            <Panel />
+            <About />
+            {status === true ? categoria.map((categoria, index) => {
+              return (
+                <div
+                  id="conteudo"
+                  className="w-full flex flex-col justify-center items-center lg:items-start"
+                  key={index}
+                >
+                  <h3 className="text-yellow-500 font-cherrybomb tracking-wide w-full my-3 text-3xl text-center md:text-left p-2 rounded">
+                    {categoria[0].toUpperCase() + categoria.substring(1)}
+                  </h3>
+                  <div className="w-full flex justify-center sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-1 flex-wrap">
+                    {produtos.map((item) => {
+                      if (item.categoria == categoria) {
+                        return (
+                          <div
+                            key={item.id}
+                            className="bg-gray-100 border w-full flex flex-col justify-center items-center rounded p-2 h-32"
+                          >
+                            <span className="w-full tracking-widest text-lg font-bold text-gray-800 opacity-80">
+                              {item.nome[0].toUpperCase() +
+                                item.nome.substring(1)}
+                            </span>
+                            <span className="text-gray-500 tracking-wide w-full h-full">
+                              {item.descricao[0].toUpperCase() +
+                                item.descricao.substring(1)}
+                            </span>
+                            <span className="w-full tracking-widest text-right whitespace-nowrap text-lg text-gray-800 font-bold opacity-80">
+                              R$ {item.preco.toFixed(2).replace(".", ",")}
+                            </span>
+                          </div>
+                        );
+                      }
+                    })}
+                  </div>
                 </div>
-              </div>
-            );
-          }) : <span className="text-gray-100">{status}</span>}
+              );
+            }) : <span className="text-gray-100">{status}</span>}
+          </div>
           <Footer />
         </main>
       </div>
