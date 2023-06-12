22933bf652e6b61dbb7bd7e7b1f8d6bc2746e486
Henrique de Moraes Botelho da Silva <henriquebotelho.imp@gmail.com>
2023-06-12 08:58:36 -0300

    feat: links para redes sociais

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -14,29 +14,65 @@ function Footer() {
         <div className="w-full">
           <h3 className="w-full text-gray-300 text-lg pb-3">Desenvolvedores</h3>
           <ul className="grid grid-cols-2 grid-rows-5 md:grid-cols-3">
-            <li className="text-gray-500">Abner</li>
-            <li className="text-gray-500">Álvaro</li>
-            <li className="text-gray-500">Bianca</li>
-            <li className="text-gray-500">Eduarda</li>
-            <li className="text-gray-500">Fabiola</li>
-            <li className="text-gray-500">Gabriel</li>
-            <li className="text-gray-500">Giovanna</li>
-            <li className="text-gray-500">Guilherme Cunha</li>
-            <li className="text-gray-500">Guilherme Leoni</li>
-            <li className="text-gray-500">Gustavo Alves</li>
-            <li className="text-gray-500">Gustavo Lima</li>
-            <li className="text-gray-500">Heitor</li>
-            <li className="text-gray-500">Henrique Botelho</li>
-            <li className="text-gray-500">Henrique Lobo</li>
-            <li className="text-gray-500">Luiz</li>
+            <a href="https://github.com/ClaushSouza" target="_blank">
+              <li className="text-gray-500">Abner Mendes</li>
+            </a>
+            <a href="https://github.com/alvarexx" target="_blank">
+              <li className="text-gray-500">Álvaro Muniz</li>
+            </a>
+            <a href="https://github.com/BiancaMarcondes" target="_blank">
+              <li className="text-gray-500">Bianca Marcondes</li>
+            </a>
+            <a href="https://github.com/dudaribas" target="_blank">
+              <li className="text-gray-500">Eduarda Ribas</li>
+            </a>
+            <a href="https://github.com/fabiola-girotti" target="_blank">
+              <li className="text-gray-500">Fabiola Girotti</li>
+            </a>
+            <a href="https://github.com/Gctyx" target="_blank">
+              <li className="text-gray-500">Gabriel Cordeiro</li>
+            </a>
+            <a href="https://github.com/GiPaiva" target="_blank">
+              <li className="text-gray-500">Giovanna Paiva</li>
+            </a>
+            <a href="https://github.com/Guigite" target="_blank">
+              <li className="text-gray-500">Guilherme Cunha</li>
+            </a>
+            <a href="https://github.com/GuiLeoni" target="_blank">
+              <li className="text-gray-500">Guilherme Leoni</li>
+            </a>
+            <a href="https://github.com/Decompilationist" target="_blank">
+              <li className="text-gray-500">Gustavo Alves</li>
+            </a>
+            <a href="https://github.com/gustavol1" target="_blank">
+              <li className="text-gray-500">Gustavo Lima</li>
+            </a>
+            <a href="https://github.com/heitorsclaudino" target="_blank">
+              <li className="text-gray-500">Heitor Claudino</li>
+            </a>
+            <a href="https://github.com/Henrique-Botelho" target="_blank">
+              <li className="text-gray-500">Henrique Botelho</li>
+            </a>
+            <a href="https://github.com/HLN-lobo" target="_blank">
+              <li className="text-gray-500">Henrique Lobo</li>
+            </a>
+            <a href="https://github.com/LAugustoLeite" target="_blank">
+              <li className="text-gray-500">Luiz Augusto</li>
+            </a>
           </ul>
         </div>
         <div className="w-full sm:w-96 flex flex-col">
           <h3 className="w-full text-gray-300 text-lg pb-3">Redes Sociais</h3>
           <div className="h-full flex gap-1">
-            <AiOutlineInstagram size={20} className="text-gray-500" />
-            <AiOutlineFacebook size={20} className="text-gray-500" />
-            <AiOutlineGithub size={20} className="text-gray-500" />
+            <a href="https://www.instagram.com/senai.suico/" target="_blank">
+              <AiOutlineInstagram size={20} className="text-gray-500" />
+            </a>
+            <a href="https://www.facebook.com/senai.suico/?locale=pt_BR" target="_blank">
+              <AiOutlineFacebook size={20} className="text-gray-500" />
+            </a>
+            <a href="https://github.com/Senai-Suico-Turma-3dmA" target="_blank">
+              <AiOutlineGithub size={20} className="text-gray-500" />
+            </a>
           </div>
         </div>
       </div>
